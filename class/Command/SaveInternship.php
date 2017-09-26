66975ee4f52c338112c577563c505fbdbfb3b207
Olivia Perugini <oliviaperugini@gmail.com>
2017-09-26 17:10:50 -0400

    Work in progress. Term isn't working for adding a student.

--- class/Command/SaveInternship.php
+++ class/Command/SaveInternship.php
@@ -151,7 +151,6 @@ class SaveInternship {
 
 
         // Record last modified time of the internship and user who modified it.
-        //date_default_timezone_set("");
         $i->last_mod_time = $_SERVER['REQUEST_TIME'];
         $i->last_mod_time_user = \Current_User::getUserObj();
 
